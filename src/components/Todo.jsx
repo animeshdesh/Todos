aed014f48d5ca6a65cd219bdfe269c1ba5e4dd51
animeshdesh <animeshdesh.aa@gmail.com>
2023-09-04 12:12:32 +0530

    Added more features

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,37 @@
 import { useEffect, useState } from "react";
 import SingleTodo from "./SingleTodo";
 import AddTodoForm from "./AddTodoForm";
-import { Container, List, Typography } from "@mui/material";
+import { Container, List, Typography, Button, Box } from "@mui/material";
+import Snackbar from "@mui/material/Snackbar";
+
+function getTodaysDate() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = (today.getMonth() + 1).toString().padStart(2, "0"); // Month is zero-based
+  const day = today.getDate().toString().padStart(2, "0");
+  const formattedDate = `${year}/${month}/${day}`;
+
+  return formattedDate;
+}
 
 const Todo = () => {
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
   const [todos, setTodos] = useState(() => {
     const storageData = JSON.parse(localStorage.getItem("todos"));
     if (!storageData) return [];
     return storageData;
   });
 
+  const todayDate = getTodaysDate();
+  const openSnackbar = (message) => {
+    setSnackbarMessage(message);
+
+    setSnackbarOpen(true);
+  };
+
   const handelRemove = (id) => {
+    openSnackbar("Deleted Todo");
     setTodos((prevtodos) => {
       return prevtodos.filter((t) => t.id !== id);
     });
@@ -26,9 +47,11 @@ const Todo = () => {
         }
       });
     });
+    openSnackbar(`Checked`);
   };
 
   const handelAddTodo = (text) => {
+    openSnackbar("Added todo successfully");
     setTodos((prevtodos) => {
       return [
         ...prevtodos,
@@ -37,13 +60,32 @@ const Todo = () => {
     });
   };
 
+  const handelDeleteAllSelected = () => {
+    setTodos((prevtodo) => {
+      const updatedTodos = prevtodo.filter((todo) => !todo.checked);
+      openSnackbar("Selected todos deleted successfully");
+      return updatedTodos;
+    });
+  };
+
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
+
   return (
     <>
       <Container>
-        <Typography variant="h2">Todo List</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h2">Todo List</Typography>
+          <Typography variant="h4">{todayDate}</Typography>
+        </Box>
+
         <List
           sx={{
             width: "100%",
@@ -69,6 +111,20 @@ const Todo = () => {
           })}
         </List>
         <AddTodoForm handelAddTodo={handelAddTodo} />
+        <Button
+          variant="outlined"
+          color="error"
+          sx={{ marginTop: "25px" }}
+          onClick={handelDeleteAllSelected}
+        >
+          Delete Selected
+        </Button>
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={5000} // Adjust the duration as needed
+          onClose={() => setSnackbarOpen(false)}
+          message={snackbarMessage}
+        />
       </Container>
     </>
   );
